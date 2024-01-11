2c004b3fd37509d48b71dd18a3c94085b0578cbe
RomeroLeandro <romerodisind@gmail.com>
2024-01-11 20:39:15 -0300

    create component contact, decompse component proyects

---
+++ src/database/projects.ts
@@ -0,0 +1,113 @@
+import { TAGS } from "./tags";
+export const PROJECTS = [
+  {
+    title: "Portfolio V1",
+    description:
+      "Atractivo portfolio personal con HTML, CSS, JavaScript y SCSS, como proyecto final del curso de Desarrollo Web.",
+    link: "https://portfolioleandroromero.vercel.app/",
+    github: "https://github.com/RomeroLeandro/PortfolioV1",
+    image: "/projects/portfolio-v1.png",
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.JAVASCRIPT, TAGS.SASS, TAGS.BOOTSTRAP],
+    important: false,
+  },
+  {
+    title: "Romero Desing",
+    description:
+      "Ecommerce funcional con experiencia de compra intuitiva y sin complicaciones. HTML, CSS y JavaScript utilizados.",
+    link: "https://romero-desing.vercel.app/",
+    github: "https://github.com/RomeroLeandro/RomeroDesing-ecommerce",
+    image: "/projects/romero-desing.png",
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.JAVASCRIPT],
+    important: false,
+  },
+  {
+    title: "Dixer Gaming",
+    description:
+      "Ecommerce con React, Redux y Firebase para el manejo de estado y almacenamiento de datos. Navegación y filtrado intuitivos. Carrito de compras funcional. Diseño con CSS y Bootstrap.",
+    link: "https://dixer-gaming.vercel.app/",
+    github: "https://github.com/RomeroLeandro/Dixer-gaming-ecommerce",
+    image: "/projects/dixer-gaming.png",
+    tags: [TAGS.REACT, TAGS.REDUX, TAGS.FIREBASE, TAGS.CSS, TAGS.BOOTSTRAP],
+    important: true,
+  },
+  {
+    title: "Baires Trips",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://baires-trips.vercel.app/",
+    github: "https://github.com/RomeroLeandro/baires-trips-calculadora",
+    image: "/projects/baires-trips.png",
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.JAVASCRIPT],
+    important: false,
+  },
+  {
+    title: "Codeando plataforma",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://codeando-plataforma.vercel.app/auth/login",
+    github: "https://github.com/RomeroLeandro/codeando-plataforma",
+    image: "/projects/codeando-plataforma.png",
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.TYPESCRIPT, TAGS.ANGULAR],
+    important: false,
+  },
+  {
+    title: "Travel Xplorer",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://travel-xplorer.vercel.app/persons",
+    github: "https://github.com/RomeroLeandro/TravelXplorer",
+    image: "/projects/travel-xplorer.png",
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.TYPESCRIPT, TAGS.ANGULAR],
+    important: true,
+  },
+  {
+    title: "Fake Shop",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://fake-shop-kappa-two.vercel.app",
+    github: "https://github.com/RomeroLeandro/ecommerce-arg-prog-4.0",
+    image: "/projects/faker-shop.png",
+    tags: [TAGS.VITE, TAGS.REACT, TAGS.REDUX, TAGS.MATERIALUI, TAGS.STYLED],
+    important: false,
+  },
+  {
+    title: "Appartamentos",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://appartments-c12-25.vercel.app/",
+    github: "https://github.com/No-Country/c12-25-t-node-react",
+    image: "/projects/appartamentos.png",
+    tags: [TAGS.VITE, TAGS.REACT, TAGS.REDUX, TAGS.MATERIALUI, TAGS.FIREBASE],
+    important: true,
+  },
+  // TODO: Add project image and url web
+  {
+    title: "backend coderhouse",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://github.com/RomeroLeandro/Proyecto-final-backend",
+    github: "https://github.com/pheralb/svgl",
+    image: "/projects/svgl.webp",
+    tags: [
+      TAGS.HTML,
+      TAGS.CSS,
+      TAGS.JAVASCRIPT,
+      TAGS.NODEJS,
+      TAGS.EXPRESSJS,
+      TAGS.MONGODB,
+      TAGS.SOCKET,
+    ],
+    important: true,
+  },
+  // TODO: Add project image and url web
+  {
+    title: "portfolio astro",
+    description:
+      "Calculadora de costos de viaje web con interfaz intuitiva. Desarrollada en HTML, CSS y JavaScript. Escalable y adaptable.",
+    link: "https://github.com/RomeroLeandro/portfolio-astro",
+    github: "https://github.com/RomeroLeandro/portfolio-astro",
+    image: "/projects/svgl.webp",
+    tags: [TAGS.ASTROICON, TAGS.TAILWIND, TAGS.TYPESCRIPT],
+    important: true,
+  },
+];
---
+++ src/database/tags.js
@@ -0,0 +1,130 @@
+import NextJS from "../icons/NextJS.astro";
+import Tailwind from "../icons/Tailwind.astro";
+import TypeScript from "../icons/TypeScript.astro";
+import Sass from "../icons/Sass.astro";
+import Firebase from "../icons/Firebase.astro";
+import MongoDB from "../icons/MongoDB.astro";
+import ExpressJS from "../icons/ExpressJS.astro";
+import NestJS from "../icons/NestJS.astro";
+import NodeJS from "../icons/NodeJS.astro";
+import MaterialUI from "../icons/MaterialUI.astro";
+import Bootstrap from "../icons/Bootstrap.astro";
+import Astroicon from "../icons/AstroIcon.astro";
+import Vite from "../icons/Vite.astro";
+import Angular from "../icons/Angular.astro";
+import React from "../icons/React.astro";
+import JavaScript from "../icons/JavaScript.astro";
+import CSS from "../icons/CSS.astro";
+import HTML from "../icons/HTML.astro";
+import Redux from "../icons/Redux.astro";
+import Socket from "../icons/SocketIO.astro";
+
+import Styled from "../icons/Styled.astro";
+
+export const TAGS = {
+  SOCKET: {
+    name: "Socket.io",
+    class: "bg-[#01010180] text-white",
+    icon: Socket,
+  },
+  HTML: {
+    name: "HTML",
+    class: "bg-[#E34F2680] text-white",
+    icon: HTML,
+  },
+  CSS: {
+    name: "CSS",
+    class: "bg-[#1572B680] text-white",
+    icon: CSS,
+  },
+  JAVASCRIPT: {
+    name: "JavaScript",
+    class: "bg-[#F7DF1E80] text-white",
+    icon: JavaScript,
+  },
+  REACT: {
+    name: "React",
+    class: "bg-[#61DAFB80] text-white",
+    icon: React,
+  },
+  ANGULAR: {
+    name: "Angular",
+    class: "bg-[#DD003195] text-white",
+    icon: Angular,
+  },
+  VITE: {
+    name: "Vite",
+    class: "bg-[#646CFF96] text-white",
+    icon: Vite,
+  },
+  ASTROICON: {
+    name: "Astro",
+    class: "bg-[#0D111780] text-white",
+    icon: Astroicon,
+  },
+  BOOTSTRAP: {
+    name: "Bootstrap",
+    class: "bg-[#7952B380] text-white",
+    icon: Bootstrap,
+  },
+  MATERIALUI: {
+    name: "Material UI",
+    class: "bg-[#0081CB80] text-white",
+    icon: MaterialUI,
+  },
+  NODEJS: {
+    name: "Node.js",
+    class: "bg-[#33993380] text-white",
+    icon: NodeJS,
+  },
+  NESTJS: {
+    name: "NestJS",
+    class: "bg-[#E0234E80] text-white",
+    icon: NestJS,
+  },
+  EXPRESSJS: {
+    name: "Express.js",
+    class: "bg-[#00000080] text-white",
+    icon: ExpressJS,
+  },
+  MONGODB: {
+    name: "MongoDB",
+    class: "bg-[#47A24880] text-white",
+    icon: MongoDB,
+  },
+  FIREBASE: {
+    name: "Firebase",
+    class: "bg-[#FFCA2880] text-white",
+    icon: Firebase,
+  },
+  SASS: {
+    name: "Sass",
+    class: "bg-[#CC669980] text-white",
+    icon: Sass,
+  },
+  TYPESCRIPT: {
+    name: "TypeScript",
+    class: "bg-[#3178C680] text-white",
+    icon: TypeScript,
+  },
+  NEXT: {
+    name: "Next.js",
+    class: "bg-black text-white",
+    icon: NextJS,
+  },
+  TAILWIND: {
+    name: "Tailwind CSS",
+    class: "bg-[#003159] text-white",
+    icon: Tailwind,
+  },
+  REDUX: {
+    name: "Redux",
+    class: "bg-[#764ABC80] text-white",
+    icon: Redux,
+  },
+  STYLED: {
+    name: "Styled Components",
+    class: "bg-[#DB709380] text-white",
+    icon: Styled,
+  },
+};
